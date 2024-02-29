251d59acc3ecf7bb3c8173c7561729a9cbf839a0
Armand Adroher <aadroher@gmail.com>
2024-03-01 00:27:57 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
-fn day_1_problem_1(_lines: Vec<&str>) -> i32 {
-    0
+use std::char;
+
+fn day_1_problem_1(lines: Vec<&str>) -> u32 {
+    println!("{:#?}", lines);
+    lines
+        .iter()
+        .map(|line: &&str| {
+            let chars = line.chars();
+            println!("{:#?}", chars);
+            let digits: Vec<u32> = chars
+                .filter(|char: &char| char.is_ascii_digit())
+                .map(|char: char| char.to_digit(10).unwrap())
+                .collect();
+
+            let line_total: u32 = digits
+                .iter()
+                .rev()
+                .enumerate()
+                .map(|(i, n)| 10 ^ (i as u32) * n)
+                .sum();
+            line_total
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -8,9 +29,11 @@ mod tests {
 
     #[test]
     fn example() {
-        let input = vec!["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
-        let expected: i32 = 142;
-        assert!(day_1_problem_1(input) == expected)
+        let input: Vec<&str> = vec!["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
+        let expected: u32 = 142;
+        let output = day_1_problem_1(input);
+        println!("{}", output);
+        assert!(output == expected);
     }
 }
 
