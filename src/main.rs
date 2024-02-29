90cde8e66ec6f57a33a4061fe40254ab3f62e41f
Armand Adroher <aadroher@gmail.com>
2024-02-29 23:42:10 +0100

    Writes failing test.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+fn day_1_problem_1(_lines: Vec<&str>) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let input = vec!["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
+        let expected: i32 = 142;
+        assert!(day_1_problem_1(input) == expected)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
