b0e10023fd5d712ec220fece05f679a3d4f6e170
Armand Adroher <aadroher@gmail.com>
2024-05-19 01:21:02 +0200

    Uses the delete operator

--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -5,6 +5,9 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
+const :: forall a b. a -> b -> a
+const a _ = a
+
 flip :: forall a b c. (a -> b -> c) -> b -> a -> c
 flip f b a = f a b
 
--- purescript/test/Main.purs
+++ purescript/test/Main.purs
@@ -1,12 +1,12 @@
 module Test.Main where
 
-import Prelude hiding (flip)
+import Prelude hiding (flip, const)
 
 import Effect (Effect)
 import Effect.Class.Console (log)
 
-import Main (flip)
+import Main (flip, const)
 
 main :: Effect Unit
 main = do
-  log (show (flip const 1 2))
+  log $ show $ flip const 1 2
