c6f24e81354b1bf3339b526b4cc7fddbdbc39eb5
Armand Adroher <aadroher@gmail.com>
2024-05-22 00:12:12 +0200

    Adds map example to check whether the compiled object is stack safe.

--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -12,6 +12,10 @@ pow n m = n * pow n (m - 1)
 pow' :: Int -> Int -> Int
 pow' _ 0 = 1
 pow' n m = pow' n (m - 1) * n
+
+double :: Array Int -> Array Int
+double ns = map (\n -> n * 2) ns
+
 main :: Effect Unit
 main = do
   log "🍝"
