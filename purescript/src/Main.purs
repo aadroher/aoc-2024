e89537cc6c29389af7ae161c465d117eab7b0f94
Armand Adroher <aadroher@gmail.com>
2024-05-22 01:17:26 +0200

    Halfway through adding the solution for the first problem

--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -2,19 +2,19 @@ module Main where
 
 import Prelude
 
+import Control.Monad.List.Trans (filter)
+import Data.Either (Either)
 import Effect (Effect)
 import Effect.Console (log)
+import Data.Char (digitToInt)
 
-pow :: Int -> Int -> Int
-pow _ 0 = 1 
-pow n m = n * pow n (m - 1)
-
-pow' :: Int -> Int -> Int
-pow' _ 0 = 1
-pow' n m = pow' n (m - 1) * n
-
-double :: Array Int -> Array Int
-double ns = map (\n -> n * 2) ns
+parseCode :: String -> Either Int
+parseCode c = 
+  firstDigit
+  where
+    digits = parseI filter (\d -> d >= '0' && d <= '9') c 
+day1problem1 :: Array String -> Int
+day1problem1 _ = 0
 
 main :: Effect Unit
 main = do
--- purescript/test/Main.purs
+++ purescript/test/Main.purs
@@ -3,10 +3,21 @@ module Test.Main where
 import Prelude hiding (flip, const)
 
 import Effect (Effect)
+import Effect.Aff (delay, launchAff_)
 import Effect.Class.Console (log)
-import Main (pow)
+import Main (day1problem1)
+import Test.Spec (describe, it, pending)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (runSpec)
+
+
 
 main :: Effect Unit
-main = do
-  log "Hello, World!"
-  log $ show $ pow 2 2147483647
+main = launchAff_ $ runSpec [consoleReporter] do
+  describe "day 1" do
+    describe "problem 1" do
+      it "works for the example" do
+        let input = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+        let expected = 142
+        day1problem1 input `shouldEqual` expected
