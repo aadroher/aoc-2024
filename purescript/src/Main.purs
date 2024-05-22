63c0453ed482f17269dd89b225c11076a3c862a8
Armand Adroher <aadroher@gmail.com>
2024-05-23 00:45:46 +0200

    Adds first steps in creating the parse function

--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -3,16 +3,19 @@ module Main where
 import Prelude
 
 import Control.Monad.List.Trans (filter)
+import Data.Array (head, last)
 import Data.Either (Either)
+import Data.Int as Int
 import Effect (Effect)
 import Effect.Console (log)
-import Data.Char (digitToInt)
 
 parseCode :: String -> Either Int
 parseCode c = 
   firstDigit
   where
-    digits = parseI filter (\d -> d >= '0' && d <= '9') c 
+    digits = Int.fromString <$> filter (\d -> d >= '0' && d <= '9') c
+    firstDigit = head digits
+    lastDigit = last digits
 day1problem1 :: Array String -> Int
 day1problem1 _ = 0
 
